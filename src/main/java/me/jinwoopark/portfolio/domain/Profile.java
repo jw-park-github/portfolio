e8a9a67dde199db9a5c8361d1f5cc8b6e6fc90da
jw-park-github <165545220+jw-park-github@users.noreply.github.com>
2024-04-09 02:30:07 +0900

    Update Profile.java

--- src/main/java/me/jinwoopark/portfolio/domain/Profile.java
+++ src/main/java/me/jinwoopark/portfolio/domain/Profile.java
@@ -14,25 +14,25 @@ public class Profile {
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "main_image", length = 2000, nullable = false)
+    @Column(name = "main_image", length = 300, nullable = false)
     private String mainImage;
 
-    @Column(name = "about", length = 2000, nullable = false)
+    @Column(name = "about", length = 500, nullable = false)
     private String about;
 
-    @Column(name = "other_one", length = 20000, nullable = false)
+    @Column(name = "other_one", length = 300, nullable = false)
     private String otherOne;
 
-    @Column(name = "other_two", length = 20000, nullable = false)
+    @Column(name = "other_two", length = 300, nullable = false)
     private String otherTwo;
 
-    @Column(name = "other_three", length = 20000, nullable = false)
+    @Column(name = "other_three", length = 300, nullable = false)
     private String otherThree;
 
-    @Column(name = "other_four", length = 20000, nullable = false)
+    @Column(name = "other_four", length = 300, nullable = false)
     private String otherFour;
 
-    @Column(name = "other_five", length = 20000, nullable = false)
+    @Column(name = "other_five", length = 300, nullable = false)
     private String otherFive;
 
     @Column(name = "other_six", length = 20000, nullable = false)
