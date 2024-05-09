ea6c314d0db74104a6ea0b8ac67f63cd876e8d0f
jw-park-github <165545220+jw-park-github@users.noreply.github.com>
2024-05-09 19:15:01 +0900

    Update githubfinder.js

--- src/main/resources/static/js/githubfinder.js
+++ src/main/resources/static/js/githubfinder.js
@@ -32,7 +32,6 @@ class UI {
         this.profile = document.getElementById('profile');
     }
 
-    // Display profile in UI
     showProfile(user) {
         this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -61,7 +60,7 @@ class UI {
     `;
     }
 
-    // Show user repos
+
     showRepos(repos) {
         let output = '';
 
@@ -82,34 +81,26 @@ class UI {
       `;
         });
 
-        // Output repos
+       
         document.getElementById('repos').innerHTML = output;
     }
 
-    // Show alert message
+    
     showAlert(message, className) {
-        // Clear any remaining alerts
+       
         this.clearAlert();
-        // Create div
         const div  =  document.createElement('div');
-        // Add classes
         div.className = className;
-        // Add text
         div.appendChild(document.createTextNode(message));
-        // Get parent
         const container =  document.querySelector('.searchContainer');
-        // Get search box
         const search = document.querySelector('.search');
-        // Insert alert
         container.insertBefore(div, search);
 
-        // Timeout after 3 sec
         setTimeout(() => {
             this.clearAlert();
         }, 3000);
     }
 
-    // Clear alert message
     clearAlert() {
         const currentAlert = document.querySelector('.alert');
 
@@ -118,40 +109,31 @@ class UI {
         }
     }
 
-    // Clear profile
     clearProfile() {
         this.profile.innerHTML = '';
     }
 }
 
-// Init Github
-const github = new Github;
-// Init UI
-const ui = new UI;
 
-// Search input
+const github = new Github;
+const ui = new UI;
 const searchUser = document.getElementById('searchUser');
 
-// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-    // Get input text
+
     const userText = e.target.value;
 
     if(userText !== ''){
-        // Make http call
         github.getUser(userText)
             .then(data => {
                 if(data.profile.message === 'Not Found') {
-                    // Show alert
                     ui.showAlert('User not found', 'alert alert-danger');
                 } else {
-                    // Show profile
                     ui.showProfile(data.profile);
                     ui.showRepos(data.repos);
                 }
             })
     } else {
-        // Clear profile
         ui.clearProfile();
     }
 });
